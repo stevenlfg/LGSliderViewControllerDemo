d8f913e62d98fa53a91b0e3d0ee279b1673d6ed3
lifuguang <fuguang.li@519mi.com>
2015-06-08 13:45:10 +0800

    Initial Commit

---
+++ LGSliderViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGSliderViewController
+//
+//  Created by stevenlfg on 15/6/8.
+//  Copyright (c) 2015年 stevenlfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LGSliderViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LGSliderViewController
+//
+//  Created by stevenlfg on 15/6/8.
+//  Copyright (c) 2015年 stevenlfg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
